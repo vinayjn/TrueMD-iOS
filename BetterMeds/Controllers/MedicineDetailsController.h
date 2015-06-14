db46c9e2a55067596363ac90977bab85f52ef46d
Vinay Jain <vinay.jn7@gmail.com>
2015-06-14 23:23:36 +0530

    added Medicine Detail controller

---
+++ BetterMeds/Controllers/MedicineDetailsController.h
@@ -0,0 +1,15 @@
+//
+//  MdicineDetailsController.h
+//  BetterMeds
+//
+//  Created by Vinay Jain on 6/14/15.
+//  Copyright (c) 2015 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MedicineDetailsController : UIViewController
+
+@property (strong,nonatomic) NSDictionary *medicineDetails;
+
+@end
---
+++ BetterMeds/Controllers/MedicineDetailsController.m
@@ -0,0 +1,52 @@
+//
+//  MdicineDetailsController.m
+//  BetterMeds
+//
+//  Created by Vinay Jain on 6/14/15.
+//  Copyright (c) 2015 Vinay Jain. All rights reserved.
+//
+
+#import "MedicineDetailsController.h"
+
+@interface MedicineDetailsController ()
+@property (weak, nonatomic) IBOutlet UILabel *medicineName;
+@property (weak, nonatomic) IBOutlet UILabel *price;
+@property (weak, nonatomic) IBOutlet UILabel *composition;
+
+@end
+
+@implementation MedicineDetailsController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"MedicineDetailsController"];
+        
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBar.titleTextAttributes = @{ NSForegroundColorAttributeName : [UIColor whiteColor] };
+    self.title = @"Medicine Details";
+    self.medicineName.text = [self.medicineDetails valueForKeyPath:@"medicine.brand"];
+    
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
