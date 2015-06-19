0cf378386daf84b22a043a1a136aa1981f6d398a
Vinay Jain <vinay.jn7@gmail.com>
2015-06-20 01:25:30 +0530

    Minor fixes, removed unused files

--- BetterMeds/Controllers/MedicineDetailsController.h
+++ BetterMeds/Controllers/MedicineDetailsController.h
@@ -9,8 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface MedicineDetailsController : UIViewController
-
 @property (strong,nonatomic) NSDictionary *medicineDetails;
-
-@property(strong,nonatomic) NSString * medicineID;
 @end
