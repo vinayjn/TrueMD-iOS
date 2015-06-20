eacbc45b919bac0aa10b99659ec6d33b973ee6af
Vinay Jain <vinay.jn7@gmail.com>
2015-06-20 12:48:55 +0530

    Additions and Improvements :
    - Now select an alternative medicine and view its details and its alternatives
    - Added activity indicator to keep the app responsive
    - Changes to a few colors based on price of medicine alternatives

--- BetterMeds/Controllers/MedicineDetailsController.m
+++ BetterMeds/Controllers/MedicineDetailsController.m
@@ -19,7 +19,9 @@
 
 @end
 
-@interface MedicineDetailsController ()<NetworkDelegate,UITableViewDataSource,UITableViewDelegate>
+@interface MedicineDetailsController ()<NetworkDelegate,UITableViewDataSource,UITableViewDelegate>{
+    int tag;
+}
 @property (weak, nonatomic) IBOutlet UILabel *medicineName;
 @property (weak, nonatomic) IBOutlet UILabel *price;
 @property (weak, nonatomic) IBOutlet UILabel *composition;
@@ -27,7 +29,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *tablets;
 @property (weak, nonatomic) IBOutlet UILabel *tabletType;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
-
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *centralIndicator;
 @property (weak, nonatomic) IBOutlet UITableView *medicineTableView;
 
 @property (strong,nonatomic) NSArray *tableData;
@@ -49,7 +51,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [[UIApplication sharedApplication] endIgnoringInteractionEvents];
     [NetworkManager sharedInstance].delegate = self;
     
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
@@ -66,7 +67,7 @@
     
     self.price.text = [NSString stringWithFormat:@"%@ Rs",[self.medicineDetails valueForKeyPath:@"medicine.package_price"]];
     
-    self.composition.text = [NSString stringWithFormat:@"%@%@",[[self.medicineDetails valueForKeyPath:@"constituents.name"] objectAtIndex:0],[[self.medicineDetails valueForKeyPath:@"constituents.strength"] objectAtIndex:0]];
+    self.composition.text = [NSString stringWithFormat:@"%@ (%@)",[[self.medicineDetails valueForKeyPath:@"constituents.name"] objectAtIndex:0],[[self.medicineDetails valueForKeyPath:@"constituents.strength"] objectAtIndex:0]];
     
     self.tabletType.text = [self.medicineDetails valueForKeyPath:@"medicine.unit_type"];
     
@@ -98,8 +99,24 @@
     NSDictionary *data = [self.tableData objectAtIndex:indexPath.row];
     
     cell.alterName.text = [data valueForKey:@"brand"];
+    
+    
+    
+    
     cell.alterPrice.text = [NSString stringWithFormat:@"%@ Rs",[data valueForKey:@"package_price"]];
+    
+    if ([self.price.text integerValue] > [cell.alterPrice.text integerValue]) {
+        [cell.alterPrice setTextColor:[UIColor greenColor]];
+    }
+    else if ([self.price.text integerValue] < [cell.alterPrice.text integerValue]) {
+        [cell.alterPrice setTextColor:[UIColor redColor]];
+    }
+    else {
+        [cell.alterPrice setTextColor:[UIColor blueColor]];
+    }
     cell.alterTablets.text = [NSString stringWithFormat:@"%@ Tablets",[data valueForKey:@"package_qty"]];
+    
+    cell.backgroundColor = [UIColor groupTableViewBackgroundColor];
     
     return cell;
     
@@ -108,15 +125,32 @@
 #pragma mark - TableView Delegate
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
+    AlternateCell *cell = (AlternateCell*)[tableView cellForRowAtIndexPath:indexPath];
+    tag = 2;
+    [self.centralIndicator startAnimating];
+    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+    
+    [[NetworkManager sharedInstance] getMedicineDetailsForID:cell.alterName.text];
     
 }
 
 -(void)updateDataSourceWith:(id)dataSource{
     
     dispatch_async(dispatch_get_main_queue(), ^{
+        
+        if (tag == 2) {
+            [self.centralIndicator stopAnimating];
+            [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+            MedicineDetailsController *medicineDetailsController = [MedicineDetailsController new];
+            medicineDetailsController.medicineDetails = (NSDictionary *)dataSource;
+            [self.navigationController pushViewController:medicineDetailsController animated:YES];
+            
+        }
+        else{
         [self.indicator stopAnimating];
         self.tableData = (NSArray *) dataSource;
         [self.medicineTableView reloadData];
+        }
     });
 }
 
